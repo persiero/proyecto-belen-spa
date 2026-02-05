da0f6f01d9588533c2af4a2f972cdc2e9b7c1199
unknown <percyrojasrod@gmail.com>
2026-02-04 23:56:52 -0500

    icono favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,10 @@
     <title>Belen Degregori | @yield('titulo', 'Admin')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
+    {{-- FAVICON --}}
+    <link rel="icon" type="image/png" href="{{ asset('adminlte/assets/img/Logo-belen.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('adminlte/assets/img/Logo-belen.png') }}">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/styles/overlayscrollbars.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
